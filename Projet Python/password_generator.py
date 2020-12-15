bb27c79242ed72967bef0a5ffdbd4ce5662633cf
MorganHolsters <he201896@students.ephec.be>
2020-12-15 20:57:28 +0100

    Update password_generator.py
    shorten random_password()

--- Projet Python/password_generator.py
+++ Projet Python/password_generator.py
@@ -1,7 +1,5 @@
 import string, random
 
 def random_password(length):
-    caracters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(caracters) for i in range(int(length)))
-    print(password)
-random_password(3)
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for i in range(int(length)))
