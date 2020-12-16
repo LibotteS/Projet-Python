028594fb2aee7babc431e1555597b571fcb8e78c
LibotteS <64150556+LibotteS@users.noreply.github.com>
2020-12-16 10:29:53 +0100

    Update password_generator.py

--- Projet Python/password_generator.py
+++ Projet Python/password_generator.py
@@ -1,39 +1,43 @@
-import string, random                                                                                           
-                                                                                                                
-                                                                                                                
-def encrypt_password(pwd):                                                                                      
-    pass                                                                                                        
-                                                                                                                
-def random_password_cpx3():                                                                                     
-                                                                                                                
-    print('please enter the length : ')                                                                         
-    length = input('\n Users => ')                                                                              
-    characters = string.ascii_letters + string.digits + string.punctuation                                      
-    return ''.join(random.choice(characters) for i in range(int(length)))                                       
-                                                                                                                
-def random_password_cpx2():                                                                                     
-                                                                                                                
-    print('please enter your password: ')                                                                       
-    password = input('\nUsers =>')                                                                              
-    """for i in range(len(password)):                                                                           
-        if password[i] == 'a':                                                                                  
-            password[i] = '@' """                                                                               
-    print(password)                                                                                             
-                                                                                                                
-def random_password_cpx1():                                                                                     
-                                                                                                                
-    print('please enter your password: ')                                                                       
-    password = input('\nUsers =>')                                                                              
-                                                                                                                
-# length = x  complexity 1-3                                                                                    
-# dictionnaire                                                                                                  
-    # 1. entrer mdp                                                                                             
-    # 2. entrer mdp et complexifier                                                                             
-    # 3. random                                                                                                 
-                                                                                                                
-    #if mot[i] == 'a'                                                                                           
-                                                                                                                
-    # cpx = 3 > random password                                                                                 
-        # min length = 8                                                                                        
-        #gestion erreur utilisateur choisis longeur 6 et cpx 3 > erreur please .... > complexity first          
-                                                                                                                
+import string, random
+
+
+def encrypt_password(pwd):
+    pass
+
+def random_password_cpx3():
+
+    print('please enter the length : ')
+    length = input('\n Users => ')
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for i in range(int(length)))
+
+def random_password_cpx2():
+
+    print('please enter your password: ')
+    password = input('\nUsers =>')
+    new_password = ''
+    for i in range(len(password)):
+        if password[i] == 'a':
+            new_password = new_password+'@'
+        else:
+            new_password = new_password+password[i]
+
+    print(new_password)
+
+def random_password_cpx1():
+
+    print('please enter your password: ')
+    password = input('\nUsers =>')
+
+# length = x  complexity 1-3
+# dictionnaire
+    # 1. entrer mdp
+    # 2. entrer mdp et complexifier
+    # 3. random
+
+    #if mot[i] == 'a'
+
+    # cpx = 3 > random password
+        # min length = 8
+        #gestion erreur utilisateur choisis longeur 6 et cpx 3 > erreur please .... > complexity first
+
